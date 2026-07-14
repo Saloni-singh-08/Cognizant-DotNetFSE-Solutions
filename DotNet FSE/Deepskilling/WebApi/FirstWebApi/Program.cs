26c627c39bf67a49666c87f822713fc860639433
Saloni-Singh-08 <isalonisingh21>
2026-07-14 21:59:20 +0530

    Completed Web API hands-on and started Kafka setup

--- DotNet FSE/Deepskilling/WebApi/FirstWebApi/Program.cs
+++ DotNet FSE/Deepskilling/WebApi/FirstWebApi/Program.cs
@@ -2,47 +2,17 @@ using System.Text;
 using FirstWebApi.Filters;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add<CustomExceptionFilter>();
 });
 
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Enter JWT Token"
-    });
-
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
-
-// JWT Authentication
 string securityKey = "mysuperdupersecretkeyforjwt12345";
 
 var symmetricSecurityKey =
