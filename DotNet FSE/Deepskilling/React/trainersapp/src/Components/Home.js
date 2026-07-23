4af479b655377221d30732142c8e18d433ed289c
Saloni-Singh-08 <isalonisingh21>
2026-07-23 14:52:23 +0530

    Completed React Hands-on 6

---
+++ DotNet FSE/Deepskilling/React/trainersapp/src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to My Academy Trainer App</h2>
+            <p>
+                This application helps you view trainers and their expertise.
+            </p>
+        </div>
+    );
+}
+
+export default Home;
